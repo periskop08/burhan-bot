572ba95593a84bfa4ae39fc3ea1fb306a489fcbe
Burhan Seçkin <periskop@Burhan-MacBook-Air.local>
2025-06-05 01:43:51 +0300

    Çakışma çözüldü

--- main.py
+++ main.py
@@ -45,17 +45,18 @@ def webhook():
     session = HTTP(api_key=api_key, api_secret=api_secret, testnet=False)
 
     try:
-       order = session.place_order(
-    category="linear",
-    symbol=symbol,
-    side="Buy" if side.lower() == "long" else "Sell",
-    order_type="Limit",
-    qty=float(quantity),
-    price=float(entry),
-    stop_loss=float(sl),
-    take_profit=float(tp),
-    time_in_force="GoodTillCancel"
-)
+        order = session.place_order(
+            category="linear",
+            symbol=symbol,
+            side="Buy" if side.lower() == "long" else "Sell",
+            order_type="Limit",
+            qty=float(quantity),
+            price=float(entry),
+            stop_loss=float(sl),
+            take_profit=float(tp),
+            time_in_force="GoodTillCancel"
+        )
+
         print("Emir gönderildi:", order)
     except Exception as e:
         print("Emir gönderilirken hata oluştu:", e)
